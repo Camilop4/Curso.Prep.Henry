c7cefefcbbfc43585e37d6ff3aa24eba7dd6e076
github_Camilop4 <jcrp_2005@hotmail.com>
2022-04-07 19:03:15 -0500

    upgraded files

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1.05;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,8 +28,8 @@ const nuevoModulo = 21 % 5 === 1.05;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
-}
+  console.log ('str');
+} devolverString ('henry');
 
 function suma(x, y) {
   // "x" e "y" son números
